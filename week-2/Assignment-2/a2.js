57ac679c6c33261ea95240121b38388e058c3cf4
Jacky W <wong.yiu.long@gmail.com>
2020-09-20 23:00:34 +0800

    Create a2.js

---
+++ week-2/Assignment-2/a2.js
@@ -0,0 +1,21 @@
+function calculate(args){
+	let result;
+	if(args.op==="+"){
+		result=args.n1+args.n2;
+	}else if(args.op==="-"){
+		result=args.n1-args.n2;
+	}else{
+		result="Not supported";
+	}
+	return result;
+}
+console.log("Way1: 7+8")
+console.log(calculate({n1:7, n2:8, op:"+"}));
+
+var numbers = new Object();
+numbers.n1=7;
+numbers.n2=8;
+numbers.op="-";
+
+console.log("Way2: 7-8")
+console.log(calculate(numbers));
