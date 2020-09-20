bb6fb9217950446dd9a33d5d66e3d539b93b4c34
Jacky W <wong.yiu.long@gmail.com>
2020-09-20 23:00:37 +0800

    Create a3.js

---
+++ week-2/Assignment-3/a3.js
@@ -0,0 +1,22 @@
+function avg(data) {
+  var sum=0;
+  for (i=0; i < data.size; i++) {
+    sum += data.products[i].price;
+  }
+  return sum/data.size;
+}
+
+console.log(
+  avg({
+    size: 3,
+    products: [{
+      name: "Product 1",
+      price: 100
+    }, {
+      name: "Product 2",
+      price: 700
+    }, {
+      name: "Product 3",
+      price: 250
+    }]
+  }));
