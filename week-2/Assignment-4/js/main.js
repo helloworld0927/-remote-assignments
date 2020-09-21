d78c41b357e12da5a681e1c366dfa3d4f3bf19ec
Jacky W <wong.yiu.long@gmail.com>
2020-09-21 16:20:38 +0800

    Create main.js

---
+++ week-2/Assignment-4/js/main.js
@@ -0,0 +1,39 @@
+//Request 1: Click to Change Text.
+//Way 1
+// function changeText() {
+//     var change = document.getElementById("banner_text");
+//     if (change.innerHTML == "Welcome Message") {
+//         change.innerHTML = "Have a Good Time!";
+//     } else {
+//         change.innerHTML = "Welcome Message";
+//     }
+// }
+
+//Way 2
+var btn;
+let btn = {
+    change: () => {
+        bannerText.textContent = "Have FUN!"
+    },
+    open: () => {
+        document.querySelector('.sidenav_open').style.right = 0;
+    },
+    close: () => {
+        document.querySelector('.sidenav_open').style.right = '-100%';
+    },
+    action: () => {
+        document.querySelector(".second").classList.toggle("hidden");
+    }
+}
+
+const bannerText = document.querySelector('.banner_text');
+bannerText.addEventListener('click', btn.change, false);
+
+const open = document.querySelector('.sidenav');
+open.addEventListener('click', btn.open, false);
+
+const close = document.querySelector('.closeBtn');
+close.addEventListener('click', btn.close, false);
+
+const action = document.querySelector('.main_button');
+action.addEventListener('click', btn.action, false);
