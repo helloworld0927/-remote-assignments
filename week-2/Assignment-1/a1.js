402ad5414bf48424aeb3c07838b1e05bb1ae21cb
Jacky W <wong.yiu.long@gmail.com>
2020-09-21 16:36:56 +0800

    week 2

--- week-2/Assignment-1/a1.js
+++ week-2/Assignment-1/a1.js
@@ -1,12 +1,12 @@
 function max(...nums) {
-	var ans=num[0];
-	for(var i = 1 ; i<num.length; i++) {
-		if(num[i]>ans) {
-			ans=num[i];
-		}
-	}
-	return ans;
+    var ans = num[0];
+    for (var i = 1; i < num.length; i++) {
+        if (num[i] > ans) {
+            ans = num[i];
+        }
+    }
+    return ans;
 }
 
-console.log(max1(1,2,4,5));
-console.log(max1(5,2,7,1,6));
+console.log(max1(1, 2, 4, 5));
+console.log(max1(5, 2, 7, 1, 6));
--- week-2/Assignment-3/a3.js
+++ week-2/Assignment-3/a3.js
@@ -1,22 +1,22 @@
 function avg(data) {
-  var sum=0;
-  for (i=0; i < data.size; i++) {
-    sum += data.products[i].price;
-  }
-  return sum/data.size;
+    var sum = 0;
+    for (i = 0; i < data.size; i++) {
+        sum += data.products[i].price;
+    }
+    return sum / data.size;
 }
 
 console.log(
-  avg({
-    size: 3,
-    products: [{
-      name: "Product 1",
-      price: 100
+    avg({
+        size: 3,
+        products: [{
+            name: "Product 1",
+            price: 100
     }, {
-      name: "Product 2",
-      price: 700
+            name: "Product 2",
+            price: 700
     }, {
-      name: "Product 3",
-      price: 250
+            name: "Product 3",
+            price: 250
     }]
-  }));
+    }));
